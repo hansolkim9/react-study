2f19d381c00380d598225a8187ef9489c09e8aca
hansolkim9 <masicer2@gmail.com>
2024-06-12 10:31:35 +0900

    # component basic

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Greet = () => {
+    return(
+        <ul>
+            <li>사과</li>
+            <li>딸기</li>
+            <li>토마토</li>
+        </ul>
+    )
+};
+
+export default Greet;
---
+++ src/components/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Hello extends React.Component {
+
+    render() {
+
+        return(
+            <>
+                <h1>헤헤</h1>
+                <h2>히히</h2>
+            </>
+        );
+    }
+}
+
+export default Hello;
