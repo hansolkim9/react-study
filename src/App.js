a3f320251207b62aa714a0a8d2b37161f25d4ddf
hansolkim9 <masicer2@gmail.com>
2024-06-12 11:33:24 +0900

    # react props 2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,42 @@ import ExpenseItem from "./components/expenses/ExpensesItem";
 
 const App = () => {
 
+    // 서버에서 지출항목 JSON 배열을 응답받음
+    const expenses = [
+        {
+            title: '치킨',
+            price: 23000,
+            date: new Date (2024, 6 - 1, 3)
+        },
+        {
+            title: '점심식사',
+            price: 10000,
+            date: new Date (2024, 2 - 1, 4)
+        },
+        {
+            title: '저녁식사',
+            price: 20000,
+            date: new Date (2024, 6 - 1, 5)
+        },
+    ]
+
     return (
         <>
-            <ExpenseItem title={"치킨"} price={23000} date={new Date(2024, 6, 3)} />
-            <ExpenseItem title="점심식사" price={10000} date={new Date(2024, 6, 4)} />
-            <ExpenseItem title="저녁식사" price={20000} date={new Date(2024, 6, 5)} />
+            <ExpenseItem
+                title={expenses[0].title}
+                price={expenses[0].price}
+                date={expenses[0].date}
+            />
+            <ExpenseItem
+                title={expenses[1].title}
+                price={expenses[1].price}
+                date={expenses[1].date}
+            />
+            <ExpenseItem
+                title={expenses[2].title}
+                price={expenses[2].price}
+                date={expenses[2].date}
+            />
         </>
     );
 }
