c8cd0396c9fefd4ed5f2c4849cedb6a85633700f
hansolkim9 <masicer2@gmail.com>
2024-06-12 11:00:54 +0900

    # react props 1

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ const App = () => {
 
     return (
         <>
-            <ExpenseItem />
-            <ExpenseItem />
+            <ExpenseItem title={"치킨"} price={23000} date={new Date(2024, 6, 3)} />
+            <ExpenseItem title="점심식사" price={10000} date={new Date(2024, 6, 4)} />
+            <ExpenseItem title="저녁식사" price={20000} date={new Date(2024, 6, 5)} />
         </>
     );
 }
