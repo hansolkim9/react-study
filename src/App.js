ac5870b98a690d821d2b4277c7d79f7153544145
hansolkim9 <masicer2@gmail.com>
2024-06-13 12:41:34 +0900

    # 동적 리스트 렌더링

--- src/App.js
+++ src/App.js
@@ -8,21 +8,10 @@ const App = () => {
 
     // 서버에서 지출항목 JSON 배열을 응답받음
     const expenses = [
-        {
-            title: '치킨',
-            price: 23000,
-            date: new Date (2024, 6 - 1, 3)
-        },
-        {
-            title: '점심식사',
-            price: 10000,
-            date: new Date (2024, 2 - 1, 4)
-        },
-        {
-            title: '저녁식사',
-            price: 20000,
-            date: new Date (2024, 6 - 1, 5)
-        },
+        {title: '치킨', price: 23000, date: new Date (2024, 6 - 1, 3)},
+        {title: '점심식사', price: 10000, date: new Date (2023, 2 - 1, 4)},
+        {title: '찜닭', price: 20000, date: new Date (2024, 6 - 1, 5)},
+        {title: '컵라면', price: 1500, date: new Date (2022, 4 - 1, 5)}
     ]
 
 
