641c15dc40fd37c920fd398e7638938ebc1e2ef0
hansolkim9 <masicer2@gmail.com>
2024-06-20 09:21:03 +0900

    # todo 동적 리스트 렌더링

--- src/components/Todo/TodoMain.js
+++ src/components/Todo/TodoMain.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './scss/TodoMain.scss';
 import TodoItem from "./TodoItem";
 
-const TodoMain = ({ todos, onToggle, onRemove }) => {
+const DUMMY_TODOS = [
+    { id: 1, title: '리액트 공부', done: true },
+    { id: 2, title: '점심 먹기', done: true },
+    { id: 3, title: '프로젝트', done: true },
+    { id: 4, title: '과제', done: true },
+]
+
+const TodoMain = () => {
     return (
         <ul className='todo-list'>
-            {todos.map(todo => (
-                <TodoItem
-                    key={todo.id}
-                    todo={todo}
-                    onToggle={onToggle}
-                    onRemove={onRemove}
-                />
-            ))}
+            {
+                DUMMY_TODOS.map(todo => <TodoItem key={todo.id} item={todo} />)
+            }
         </ul>
     );
 };
