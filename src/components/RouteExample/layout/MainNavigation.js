a2e9f717e8f68c41a40a9d3aabe6d62816c1841c
hansolkim9 <masicer2@gmail.com>
2024-07-02 11:47:43 +0900

    # 상대경로로 링크 제어하기

--- src/components/RouteExample/layout/MainNavigation.js
+++ src/components/RouteExample/layout/MainNavigation.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './MainNavigation.module.scss';
 
 const MainNavigation = () => {
+
+    const activeFn = ({ isActive }) => {
+        // NavLink 컴포넌트에 className 프롭스에 함수를 전달하면
+        // 첫번째 파라미터에 객체정보를 제공한다.
+        // 이 객체에는 isActive 속성이 포함되어 있다.
+        return isActive ? styles.active : undefined;
+    }
+
     return (
         <header className={styles.header}>
             <nav>
                 <ul className={styles.list}>
                     <li>
-                        <Link to='/'>Home</Link>
+                        <NavLink to='' className={activeFn} end>Home</NavLink>
                     </li>
                     <li>
-                        <Link to='/products'>Products</Link>
+                        <NavLink to='products' className={activeFn}>Products</NavLink>
                     </li>
                 </ul>
             </nav>
