8edb3e67456a870303831fc997cf5bf4ce3234fd
hansolkim9 <masicer2@gmail.com>
2024-07-05 10:51:52 +0900

    # 리펙토링

--- src/components/RouteExample/components/EventForm.js
+++ src/components/RouteExample/components/EventForm.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import styles from './EventForm.module.scss';
-import { useNavigate, Form } from 'react-router-dom';
+import { useNavigate, Form, redirect } from 'react-router-dom';
 
 const EventForm = ({ method, event = {} }) => {
-
     const {
         title,
         desc: description,
@@ -30,7 +29,7 @@ const EventForm = ({ method, event = {} }) => {
 
     let formatDate;
     if (event.date) {
-        const formatDate = convertDateFormat(date);
+        formatDate = convertDateFormat(date);
     }
 
     // const { eventId: id } = useParams();
@@ -43,38 +42,43 @@ const EventForm = ({ method, event = {} }) => {
     };
 
     // const submitHandler = e => {
-    //     e.preventDefault();
-    //     console.log('submit form');
-    //
-    //     // input에 입력한 값 가져오기
-    //     const formData = new FormData(e.target);
-    //     console.log('form: ', formData.get('title'));
-    //
-    //     // 서버에 보낼 데이터
-    //     const payload = {
-    //         title: formData.get('title'),
-    //         desc: formData.get('description'),
-    //         imageUrl: formData.get('image'),
-    //         beginDate: formData.get('date'),
-    //     };
-    //
-    //     // console.log('payload: ', payload);
-    //
-    //     // 서버로 페칭
-    //     (async () => {
-    //         const response = await fetch(`http://localhost:8282/events`, {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify(payload)
-    //         });
-    //     })();
+    //   e.preventDefault();
+    //   // console.log('form이 제출됨!');
+
+    //   // input에 입력한 값 가져오기
+    //   const formData = new FormData(e.target);
+    //   // console.log('form: ', formData.get('title'));
+
+    //   // 서버에 보낼 데이터
+    //   const payload = {
+    //     title: formData.get('title'),
+    //     desc: formData.get('description'),
+    //     imageUrl: formData.get('image'),
+    //     beginDate: formData.get('date')
+    //   };
+
+    //   // console.log('payload: ', payload);
+
+    //   // 서버로 페칭
+    //   (async () => {
+    //     const response = await fetch(`http://localhost:8282/events`, {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json'
+    //       },
+    //       body: JSON.stringify(payload)
+    //     });
+
+    //     navigate('/events');
+    //   })();
     // };
 
+    // 2. action함수를 트리거하려면 일반 form을 사용하면 안되고
+    // 3. react-router-dom에서 제공하는 Form이라는 컴포넌트를 사용한다.
+    // 4. method 옵션을 설정한다.
     return (
         <Form
-            method='post'
+            method={method}
             className={styles.form}
             // onSubmit={submitHandler}
             noValidate
@@ -133,3 +137,44 @@ const EventForm = ({ method, event = {} }) => {
 };
 
 export default EventForm;
+
+
+// 서버에 갱신요청을 보내는 트리거함수
+// App.js에서 router에 설정
+export const action = async ({ request, params }) => {
+    // action 함수를 트리거하는 방법
+    // 1. form이 있는 EventForm으로 이동
+    // console.log('action함수 call!');
+
+    // console.log('req: ', request);
+
+    const formData = await request.formData();
+    // console.log(formData);
+
+    const payload = {
+        title: formData.get('title'),
+        desc: formData.get('description'),
+        imageUrl: formData.get('image'),
+        beginDate: formData.get('date'),
+    };
+
+    // console.log(payload);
+
+    let url = `http://localhost:8282/events`;
+    if (request.method === 'PATCH') {
+        url += `/${params.eventId}`;
+    }
+
+    console.log('info: ', { url, method: request.method });
+
+    const response = await fetch(url, {
+        method: request.method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    });
+
+    return redirect('/events');
+
+};
